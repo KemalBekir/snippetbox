621103bd3b10218096c92c01345ba472b2e0825d
KemalBekir <outsider.pld@gmail.com>
2024-07-29 22:06:47 +0100

    Added - tests

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -19,8 +19,12 @@ type templateData struct {
 	Snippets          []*models.Snippet
 }
 
-func humanDate(t *time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
